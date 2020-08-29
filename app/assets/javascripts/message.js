5840083e3f115db11fdfc734f244abcab6014585
hyo-ha <hyo.maeda@di-v.co.jp>
2020-08-29 17:50:19 +0900

    add create.jbuilder

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,44 @@
 $(function(){ 
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+        `<div class="Message__box">
+          <div class="Message__Info">
+            <div class="Message__username">
+              ${message.user_name}
+            </div>
+            <div class="Message__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="Message">
+            <p class="Message__content">
+              ${message.content}
+            </p>
+            <img class="Message__image" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="Message__box">
+        <div class="Message__Info">
+          <div class="Message__username">
+            ${message.user_name}
+          </div>
+          <div class="Message__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="Message">
+          <p class="Message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
   $('.Form').on('submit', function(e){
     e.preventDefault()
     let formData = new FormData(this);
@@ -8,8 +48,20 @@ $(function(){
       type: "POST",
       data: formData,
       dataType: 'json',
-      processDate: false,
+      processData: false,
       contentType: false
     })
+    .done (function(data){
+      let html = buildHTML(data);
+      $('.MessageField').append(html);
+      $('form')[0].reset();
+      $('.MessageField').animate({ scrollTop: $('.MessageField')[0].scrollHeight});
+    })
+    .always(function(){
+      $(".Form__submit").prop('disabled', false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
   });
 });
